3a33bd018d93cb0b22578d874fe38435bc6218f5
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-05-11 14:16:07 +0900

    new code file and test file included

--- src/CreditsAnalysis.jl
+++ src/CreditsAnalysis.jl
@@ -2,6 +2,7 @@ module CreditsAnalysis
 
 # Write your package code here.
 include("LoadingScores.jl")
-include("ExamScoresTransformer.jl")
+include("OMRDataTransformer.jl")
+include("ProcessingScores.jl")
 
 end
