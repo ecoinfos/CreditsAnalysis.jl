0a7b2becb3c5d1cc674c8f88ac7eea0042c80bb2
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-05-14 00:33:59 +0900

    histogram creation function added

--- src/ProcessingScores.jl
+++ src/ProcessingScores.jl
@@ -4,15 +4,16 @@ using CreditsAnalysis
 using DataFrames
 using CairoMakie
 
-export collect_results, count_correct_responses!
+export collect_results, count_correct_responses!, calculate_score_sums,
+       plot_histogram
 
 
-function collect_results(df::DataFrame)::DataFrame
-  df_res = df[occursin.("RES", df[:, :DataGroups]), :]
+function collect_results(df::DataFrame, cat::String)::DataFrame
+  row_indices = findall(row -> occursin(cat, row.DataGroups), eachrow(df))
+  df_res = df[row_indices, :]
 
   return df_res
 end
-
 
 function count_correct_responses!(
   df::DataFrame, col_prefix::String, old_new_pair::Dict{String, Int64}
@@ -32,6 +33,27 @@ function count_correct_responses!(
   end
 
   return df
+end
+
+function calculate_score_sums(df::DataFrame, col_prefix::String)::DataFrame
+  cols = filter(col -> startswith(col, col_prefix), names(df)) 
+  df[!, :ScoreSums] = sum(eachcol(df[!, cols]))
+
+  return df
+end
+
+function plot_histogram(df::DataFrame, bins::Int64, title::String, period::String)
+  fig = Figure(size=(800, 600)) 
+
+  ax = Axis(
+    fig[1, 1],
+    title = title,
+    xlabel="Anatomy & Physiology $period Scores (pts.)",
+    ylabel="Number of students (persons)"
+  )
+  hist!(ax, df.ScoreSums, bins=bins, color=:steelblue, strokewidth=0)
+
+  return fig
 end
 
 end
