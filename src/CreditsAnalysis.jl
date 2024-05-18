77ae87481e3c6f5cf7c03c224a8e964487f1dae2
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-05-18 21:06:36 +0900

    code file path momified

--- src/CreditsAnalysis.jl
+++ src/CreditsAnalysis.jl
@@ -3,7 +3,7 @@ module CreditsAnalysis
 # Write your package code here.
 include("LoadingScores.jl")
 include("PROC/OMRDataTransformer.jl")
-include("ProcessingScores.jl")
+include("PROC/ProcessingScores.jl")
 include("SubjectAchievement.jl")
 
 end
