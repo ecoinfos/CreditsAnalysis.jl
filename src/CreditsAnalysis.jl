a7b3f0ffe0a96645c03d0ce1463b4daea6b5b3b0
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-06-07 01:00:04 +0900

    quiz time spent module added

--- src/CreditsAnalysis.jl
+++ src/CreditsAnalysis.jl
@@ -5,6 +5,7 @@ include("PROC/QuizTransformer.jl")
 include("PROC/OMRDataTransformer.jl")
 include("PROC/ProcessingScores.jl")
 include("PROC/SubjectAchievement.jl")
+include("PROC/ProcessingQuiz.jl")
 include("UI/CreatePlots.jl")
 
 end
