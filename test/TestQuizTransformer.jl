fe28bd3800666646187fa857d600870a16e7e7d8
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-07-05 00:38:00 +0900

    module acronyms changed in the code

--- test/TestQuizTransformer.jl
+++ test/TestQuizTransformer.jl
@@ -3,17 +3,17 @@ using CSV
 using DataFrames
 using Test
 
-import CreditsAnalysis.QuizTransformer as QT 
+import CreditsAnalysis.LoadQuiz as LQ
 
 
 @testset "CSV file loading test" begin
   test_csv1 = "test/test_csv1.csv"
   #test_csv1 = joinpath(@__DIR__, "test_csv1.csv")
   expected_df_from_csv = DataFrame(SN=[1, 2, 3], col1 = [1, 2, 3], col2 = [1, 2, 3])
-  @test QT.read_csv_to_dataframe(test_csv1) == expected_df_from_csv
+  @test LQ.read_csv_to_dataframe(test_csv1) == expected_df_from_csv
 
   test_csv2 = joinpath(@__DIR__, "test_csv1.xlsx")
-  @test_throws ErrorException QT.read_csv_to_dataframe(test_csv2)
+  @test_throws ErrorException LQ.read_csv_to_dataframe(test_csv2)
 
 
 
@@ -29,7 +29,7 @@ end
     #joinpath(@__DIR__, "test_csv1.csv"),
     #joinpath(@__DIR__, "test_csv2.csv")
   ]
-  df_dict1 = QT.read_multiple_csvs_to_dataframes(test_data_paths1)
+  df_dict1 = LQ.read_multiple_csvs_to_dataframes(test_data_paths1)
   expected_test_dfA = DataFrame(SN = 1:3, col1 = [1, 2, 3], col2 = [1, 2, 3])
   expected_test_dfB = DataFrame(SN = 1:3, col1 = [4, 5, 6], col2 = [4, 5, 6])
   @test df_dict1[:dfA] == expected_test_dfA
@@ -40,7 +40,7 @@ end
     joinpath(@__DIR__, "test_csv2.csv"),
     joinpath(@__DIR__, "test_csv1_duplicated.csv")
   ]
-  @test_throws ArgumentError QT.read_multiple_csvs_to_dataframes(test_data_paths2)
+  @test_throws ArgumentError LQ.read_multiple_csvs_to_dataframes(test_data_paths2)
 end
 
 @testset "DataFrames column titles comparison test" begin
@@ -49,14 +49,14 @@ end
   test_dfC1 = DataFrame(SN = 1:2, col1 = [5, 6], col2 = [5, 6])
 
   df_dict1 = Dict(:dfA => test_dfA1, :dfB => test_dfB1, :dfC => test_dfC1)
-  @test_logs (:info, "Column titles of dfB are same to dfA's column titles.") (:info, "Column titles of dfC are same to dfA's column titles.") QT.column_titles_comparison(df_dict1)
+  @test_logs (:info, "Column titles of dfB are same to dfA's column titles.") (:info, "Column titles of dfC are same to dfA's column titles.") LQ.column_titles_comparison(df_dict1)
 
   test_dfA2 = DataFrame(SN = 1:2, col1 = [1, 1], col2 = [2, 2])
   test_dfB2 = DataFrame(SN = 1:2, col1 = [3, 3], col2 = [4, 4])
   test_dfC2 = DataFrame(SN = 1:2, col2 = [5, 6], col3 = [5, 6])
 
   df_dict2 = Dict(:dfA => test_dfA2, :dfB => test_dfB2, :dfC => test_dfC2)
-  @test_throws ErrorException QT.column_titles_comparison(df_dict2)
+  @test_throws ErrorException LQ.column_titles_comparison(df_dict2)
 end
 
 @testset "Merge dataframes test" begin
@@ -73,13 +73,13 @@ end
   class = "A"
   
   test_data_paths1=["test/test_csv1.csv", "test/test_csv2.csv"]
-  df_dict1 = QT.read_multiple_access_csvs_to_dict(
+  df_dict1 = LQ.read_multiple_access_csvs_to_dict(
     test_data_paths1, class, start_week
   )
-  @test typeof(df_dict) == Dict{Symbol, DataFrame}
+  @test typeof(df_dict1) == Dict{Symbol, DataFrame}
   
   test_data_paths2=["test/test_csv1.csv", "test/test_csv1.csv"]
-  @test_throws ErrorException QT.read_multiple_access_csvs_to_dict(
+  @test_throws ErrorException LQ.read_multiple_access_csvs_to_dict(
     test_data_paths2, class, start_week
   )
 end
