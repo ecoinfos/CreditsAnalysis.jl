58412b443e9477ea37ce0e0ccdee72e512df9919
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-07-05 00:21:39 +0900

    quiz data processing module name changed

--- src/CreditsAnalysis.jl
+++ src/CreditsAnalysis.jl
@@ -4,7 +4,7 @@ module CreditsAnalysis
 include("PROC/LoadOMR.jl")
 include("PROC/LoadQuiz.jl")
 include("PROC/TopicStats.jl")
-include("PROC/ProcessingQuiz.jl")
+include("PROC/PrepQuiz.jl")
 include("UI/CreatePlots.jl")
 
 end
