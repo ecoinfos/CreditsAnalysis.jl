304653a8a89beb681451fcb54f3789bbad02485b
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-10-26 12:01:12 +0900

    quiz purpose type changed from exploration to application

--- src/PROC/PrepQuiz.jl
+++ src/PROC/PrepQuiz.jl
@@ -29,26 +29,26 @@ function create_quiz_question_time_dict(
   
   # Filter quiz data by `Purposes` data 
   df_understanding = filter(row -> row.Purposes == "이해", df_quiz_joined)
-  df_exploration = filter(row -> row.Purposes == "탐색", df_quiz_joined)
+  df_application = filter(row -> row.Purposes == "적용", df_quiz_joined)
   
   # Calculate means of time for every question for all students
   df_understanding_time_avg = combine(
     groupby(df_understanding, [:Weeks, :IDs]), :Time => mean => :AvgTime
   )
-  df_exploration_time_avg = combine(
-    groupby(df_exploration, [:Weeks, :IDs]), :Time => mean => :AvgTime
+  df_application_time_avg = combine(
+    groupby(df_application, [:Weeks, :IDs]), :Time => mean => :AvgTime
   )
 
   # Filter student_id daata
   df_understanding_time_avg_student = filter(row -> row.IDs == student_id, df_understanding_time_avg)
-  df_exploration_time_avg_student = filter(row -> row.IDs == student_id, df_exploration_time_avg)
+  df_application_time_avg_student = filter(row -> row.IDs == student_id, df_application_time_avg)
 
   # Prepare dictionary containing the dataframes
   dict_quiz_purposes = Dict(
     "ua" => df_understanding_time_avg,
-    "ea" => df_exploration_time_avg,
+    "aa" => df_application_time_avg,
     "us" => df_understanding_time_avg_student,
-    "es" => df_exploration_time_avg_student
+    "as" => df_application_time_avg_student
   )
 
   return dict_quiz_purposes
