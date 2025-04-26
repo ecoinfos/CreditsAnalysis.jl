5f94604f8a80a38e97f4932ad9dbb7cbc0aaabe5
Documenter.jl <documenter@juliadocs.github.io>
2025-04-26 16:11:33 +0000

    build based on 4f27264

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CreditsAnalysis","category":"page"},{"location":"#CreditsAnalysis","page":"Home","title":"CreditsAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CreditsAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CreditsAnalysis]","category":"page"}]
+[{"location":"#CreditsAnalysis","page":"Home","title":"CreditsAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CreditsAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
